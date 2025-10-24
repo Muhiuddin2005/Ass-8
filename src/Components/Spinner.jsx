e03cb91ada2c505d8f02d1cce9681f2b82c481db
Muhiuddin <muhiuddin.bd.2005@gmail.com>
2025-10-24 17:05:01 +0600

    assignment completed!

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spinner = () => {
     return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center h-100">
       <div className="w-12 h-12 border-4 border-gray-300 border-t-green-500 rounded-full animate-spin"></div>
     </div>
   )
